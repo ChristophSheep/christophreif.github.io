0663042fc0856893c42f17bb82ff6ce7dfd268e2
Christoph Reif <christoph.reif@gmail.com>
2022-06-14 20:46:28 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,11 @@
-(function() {
-
-  function create() {
-    var menu = document.getElementById("menu")
-    var a = document.createElement('a');
-    var text = document.createTextNode("bio");
-    a.appendChild(text);
-    a.title = "my title text";
-    a.href = "http://example.com";
-    menu.appendChild(a);
-  }
+function create() {
+  var menu = document.getElementById("menu")
+  var a = document.createElement('a');
+  var text = document.createTextNode("bio");
+  a.appendChild(text);
+  a.title = "my title text";
+  a.href = "http://example.com";
+  menu.appendChild(a);
+}
   
-  create();
-  
-  alert("Hello");
-  
-})()
+create();
