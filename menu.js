5ae0e09f8cb8049b8b23a2598abd0d49416d43e3
Christoph Reif <christoph.reif@gmail.com>
2022-06-16 16:37:10 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,33 +1,35 @@
-var menu = document.getElementById("menu");
-
-var items = {
-
-    "cel": "./cel.html",
-    "bio": "./bio.html",
-  "books": "./books.html",
- "essays": "./essays.html",
-"history": "./hist.html",
- "poeple": "./people.html",
- "quotes": "./quotes.html,
-};
-
 function createItem(name, link) {
-  var a = document.createElement('a');
-  a.innerHTML = name;
-  a.href = link;
- 
-  var li = document.createElement('li');
-  li.appendChild(a);
- 
-  return li
+    var a = document.createElement('a');
+    a.innerHTML = name;
+    a.href = link;
+
+    var li = document.createElement('li');
+    li.appendChild(a);
+
+    return li;
 }
 
 function createMenu() {
-   Object
-      .keys(items)
-      .forEach( function(key) { 
-         menu.appendChild(createItem(key, items[key]));
-      });
+
+    var items = {
+        "cel": "./cel.html",
+        "bio": "./bio.html",
+        "books": "./books.html",
+        "essays": "./essays.html",
+        "history": "./hist.html",
+        "poeple": "./people.html",
+        "quotes": "./quotes.html,
+    };
+
+    let nav = document.getElementById('menu');
+    var ul = document.createElement('ul');
+    nav.appendChild(ul);
+
+    Object
+        .keys(items)
+        .forEach(function(key) {
+            ul.appendChild(createItem(key, items[key]));
+        });
 }
-  
+
 createMenu();
